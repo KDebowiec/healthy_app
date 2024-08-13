d2e7a14397516eac9ec288b65686f40211eb61ca
KDebowiec <karoldebowiec@gmail.com>
2024-08-13 22:05:20 +0200

    'fkn'

--- healthy_app/users/ajax_datatable_views.py
+++ healthy_app/users/ajax_datatable_views.py
@@ -12,6 +12,7 @@ class NutritionAjaxDatatableView(AjaxDatatableView):
     # initial_order = [["datetime", "asc"], ]
     length_menu = [[10, 20, 50, 100], [10, 20, 50, 100]]
     search_values_separator = '+'
+    meal_plan = MealPlan.meal_json
 
     column_defs = [
         # AjaxDatatableView.render_row_tools_column_def(),
@@ -27,12 +28,13 @@ class NutritionAjaxDatatableView(AjaxDatatableView):
         user = self.request.user
         return self.model.objects.filter(user=user)
 
-    def render_column(self, row, column):
+    def render_column(self, row, column, meal_plan=MealPlan.meal_json):
         value = self.column_obj(column).render_column(row)
 
-        if column == 'title':
-            return render_to_string('app/notifications_list/title.html', {'object': row})
-
+        if column == 'page_title':
+            # return render_to_string('users/show_plans.html', {'object': meal_plan})
+            return f'<a href="{'/users/show_plans.html'}">{column}</a>'
+        return super().render_column(row, column)
 
 class WorkoutsAjaxDatatableView(AjaxDatatableView):
 
