7dc7888393a83c91d0d9ee4f66c2f4d531c91814
KDebowiec <karoldebowiec@gmail.com>
2024-08-19 11:36:42 +0200

    'problems'

--- healthy_app/users/ajax_datatable_views.py
+++ healthy_app/users/ajax_datatable_views.py
@@ -9,10 +9,8 @@ class NutritionAjaxDatatableView(AjaxDatatableView):
 
     model = MealPlan
     title = 'MealPlans'
-    # initial_order = [["datetime", "asc"], ]
     length_menu = [[10, 20, 50, 100], [10, 20, 50, 100]]
     search_values_separator = '+'
-    meal_plan = MealPlan.meal_json
 
     column_defs = [
         # AjaxDatatableView.render_row_tools_column_def(),
@@ -21,6 +19,7 @@ class NutritionAjaxDatatableView(AjaxDatatableView):
         {'name': 'general_min_kcal', 'visible': True, },
         {'name': 'general_max_kcal', 'visible': True, },
         {'name': 'page_title', 'visible': True, 'searchable': True},
+        {'name': 'meal_json', 'visible': True, 'searchable': False, 'orderable': False}
     ]
 
     def get_initial_queryset(self, request=None):
@@ -28,13 +27,13 @@ class NutritionAjaxDatatableView(AjaxDatatableView):
         user = self.request.user
         return self.model.objects.filter(user=user)
 
-    def render_column(self, row, column, meal_plan=MealPlan.meal_json):
-        value = self.column_obj(column).render_column(row)
+    def render_column(self, row, column):
+        # value = self.column_obj(column).render_column(row)
 
-        if column == 'page_title':
-            # return render_to_string('users/show_plans.html', {'object': meal_plan})
-            return f'<a href="{'/users/show_plans.html'}">{column}</a>'
+        if column == 'title':
+            return render_to_string('users/plan_table.html', {'meal_plan': row, 'meal_json': row.meal_json})
         return super().render_column(row, column)
+
 
 class WorkoutsAjaxDatatableView(AjaxDatatableView):
 
